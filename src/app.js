8dc237401a498edc0865b1991753e6050c3bf227
BhanuThapa <bhanuthapa252@gmail.com>
2023-02-16 00:03:43 +0530

    adding hbs, partials and 404 error page

--- src/app.js
+++ src/app.js
@@ -1,28 +1,50 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 const app = express();
 const port = process.env.PORT || 8000;
 
 // public static path
 const static_path = path.join(__dirname, '../public');
+const template_path = path.join(__dirname, '../templates/views');
+const partials_path = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
 
 app.use(express.static(static_path));
 
 // routing
-app.get('/', (req, res) => {
-  res.send('Hello this is Weather Application');
+
+// app.get('/', (req, res) => {
+//   res.send('Hello this is Weather Application');
+// });
+
+app.get('', (req, res) => {
+  res.render('index');
 });
+
+// app.get('/about', (req, res) => {
+//   res.send('Welcome to about us Page');
+// });
 
 app.get('/about', (req, res) => {
-  res.send('Welcome to about us Page');
+  res.render('about');
 });
 
+// app.get('/weather', (req, res) => {
+//   res.send('Welcome to weather Page');
+// });
+
 app.get('/weather', (req, res) => {
-  res.send('Welcome to weather Page');
+  res.render('weather');
 });
 
 app.get('*', (req, res) => {
-  res.send('404 Error, Page Not Found');
+  res.render('404error', {
+    error: 'Oops ! Page Not Found',
+  });
 });
 
 app.listen(port, () => {
